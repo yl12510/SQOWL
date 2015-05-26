dfbd49a3a25f7a8e1340133fd035ea027910ef87
yl12510 <yu.liu8904@gmail.com>
2015-05-26 10:44:35 +0100

    add python file which handles transitive inference

---
+++ src/main/python/transitive_closure.py
@@ -0,0 +1,41 @@
+import sys
+from random import Random
+
+from pyspark import SparkContext
+
+num_edges = 3000
+num_vertices = 500
+rand = Random(42)
+
+def generate_graph():
+    edges = set()
+    while len(edges) < num_edges:
+        src = rand.randrange(0, num_vertices)
+        dst = rand.randrange(0, num_vertices)
+        if src != dst:
+            edges.add((src,dst))
+    return edges
+
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        print >> sys.stderr, "Usage: transitive closure <master> [<slices>]"
+        #exit(1)
+    sc = SparkContext(sys.argv[1], "TransitiveClosure")
+    slices = int(sys.argv[2]) if len(sys.argv) > 2 else 1
+    closure = sc.parallelize(generate_graph(), slices).cache()
+
+    edges = closure.map(lambda (x,y): (y,x)).cache()
+
+    old_count = 0L
+    new_count = closure.count()
+
+    while new_count != old_count:
+        new_edges = closure.join(edges).map(lambda (_, (a,b)): (b,a))
+        new_closure = closure.union(new_edges).distinct().count()
+
+        closure.unpersist()
+        closure = new_closure
+    closure.unpersist()
+    edges.unpersist()
+
+    print "TC has %i edges" % closure.count()
